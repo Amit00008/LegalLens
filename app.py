6c84a8838d9e0c4c479926556312c97aba8eaed4
Amit <amitdey1350@gmail.com>
2025-07-06 08:14:49 +0530

    dd

--- app.py
+++ app.py
@@ -1,35 +1,7 @@
-from fastapi import FastAPI, HTTPException, Request, Header
-from pydantic import BaseModel
-from pipeline import analyze_legal_text
-from dotenv import load_dotenv
-import os
+from fastapi import FastAPI
 
-# Load Environment Variables
-load_dotenv()
+app = FastAPI()
 
-#  Read API Keys from .env
-API_KEYS = os.getenv("API_KEYS", "").split(",")
-
-app = FastAPI(title="Legal Document Analyzer API")
-
-
-
-# Request Body
-class LegalTextRequest(BaseModel):
-    legal_text: str
-
-# API Route with API Key check
-@app.post("/analyze")
-async def analyze(
-    request: LegalTextRequest, 
-    api_key: str = Header(None)  # Read from Header 'api-key'
-):
-    # Check API Key
-    if api_key not in API_KEYS:
-        raise HTTPException(status_code=401, detail="Invalid or missing API Key.")
-    
-    if not request.legal_text.strip():
-        raise HTTPException(status_code=400, detail="Empty legal text provided.")
-    
-    result = analyze_legal_text(request.legal_text)
-    return result
+@app.get("/")
+def root():
+    return {"message": "Hello from Railway!"}
